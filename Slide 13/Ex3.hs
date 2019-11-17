db0c5d3a21f74fed085871558da0b69c2664c3a6
Yaboku <leonardodrade@gmail.com>
2019-11-16 21:51:45 -0300

    Ex3

---
+++ Slide 13/Ex3.hs
@@ -0,0 +1,36 @@
+data Cmd = Atr String Exp
+        |  Seq [Cmd]
+        deriving Show 
+data Exp = Num Int
+        |   Add Exp Exp 
+        |   Sub Exp Exp
+        |   Var String
+        deriving Show
+a= Add (Num 1) (Num 2)
+c0= Atr "x" (Num 1)
+c1= Atr "x" (Add (Num 1) (Sub (Var "x") (Num 20)))
+
+memory = []
+type Mem = [(String,Int)]
+avaliaExp ::Mem ->Exp -> Int
+avaliaExp mem (Num j)= j
+avaliaExp mem (Add a b) = (avaliaExp mem a) + (avaliaExp  mem b)
+avaliaExp mem (Sub a b) = (avaliaExp mem a) - (avaliaExp  mem b) 
+avaliaExp mem (Var id) = consulta mem id
+
+consulta ::Mem -> String -> Int
+consulta [] id = 0
+consulta ((id',aux'):l) id = if id == id' then aux' else consulta l id
+
+escreve:: Mem -> String -> Int -> Mem
+escreve mem id exp = (id,exp):mem
+avaliaCmd:: Mem -> Cmd -> Mem
+avaliaCmd mem (Atr id exp) = escreve mem id (avaliaExp mem exp)
+avaliaCmd mem (Seq [])= escreve mem "" 0
+avaliaCmd mem (Seq (a:l)) = avaliaCmd mem' a where
+                              mem'= avaliaCmd mem (Seq l)
+--c2= Seq (Atr "x" (Num 1)) (Seq (Atr "y" (Num 2)) (Atr "z" ( Add (Var "x") (Var "y"))))
+c3= Atr "x" (Add (Var "x") (Num 20))
+c4= Seq [Atr "y" (Num 10),Atr "x" (Num 11)]
+--c5 = Atr "x" (Var "x")
+main = print (avaliaCmd memory c4)
